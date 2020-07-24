c245bfc91bb97efccdc5abd1882970d011fce34f
Amirhossein Ghorbani <amirhosseinghorbani@hotmail.com>
2020-07-24 05:47:57 -0400

    Add file manager exception to contain any exception for file manager package

---
+++ src/Magenta.FileManager.Abstractions/FileManagerException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Magenta.FileManager.Abstractions
+{
+    public class FileManagerException : Exception
+    {
+        public FileManagerException(string message) : base(message)
+        {
+        }
+
+        public FileManagerException(string message, Exception innerException) 
+            : base(message, innerException)
+        {
+        }
+    }
+}

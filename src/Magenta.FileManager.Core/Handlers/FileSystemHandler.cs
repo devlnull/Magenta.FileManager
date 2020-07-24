f25e501f1128c78df59f13bc67a8feec7539e3b2
Amirhossein Ghorbani <amirhosseinghorbani@hotmail.com>
2020-07-24 06:20:16 -0400

    Add base functions to fsys handler

--- src/Magenta.FileManager.Core/Handlers/FileSystemHandler.cs
+++ src/Magenta.FileManager.Core/Handlers/FileSystemHandler.cs
@@ -1,10 +1,27 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace Magenta.FileManager.Core.Handlers
 {
     public class FileSystemHandler
     {
+        public byte[] Get(string path)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save(string path, byte[] fileBytes)
+        {
+            throw new NotImplementedException();
+        }
+        
+        public void Update(string oldPath, string newPath, byte[] newFileBytes)
+        {
+            throw new NotImplementedException();
+        }
+        
+        public void Delete(string path)
+        {
+            throw new NotImplementedException();
+        }
     }
 }

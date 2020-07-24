d5744f5fbc6dc431b636a92974a6f8d10df5c16c
Amirhossein Ghorbani <amirhosseinghorbani@hotmail.com>
2020-07-24 05:48:46 -0400

    Add file input interface to use in file creating and any functionality on files from outside

---
+++ src/Magenta.FileManager.Abstractions/IFileInput.cs
@@ -0,0 +1,9 @@
+﻿namespace Magenta.FileManager.Abstractions
+{
+    public interface IFileInput
+    {
+        byte[] Content { get; set; }
+        string Extension { get; set; }
+        string Name { get; set; }
+    }
+}

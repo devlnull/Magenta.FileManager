5919dd9d9718da7ac6cc07e7d7a3cbd6e76af800
Amirhossein Ghorbani <amirhosseinghorbani@hotmail.com>
2020-07-24 06:00:19 -0400

    Add basic functionalities to file manager

--- src/Magenta.FileManager.Abstractions/IFileManager.cs
+++ src/Magenta.FileManager.Abstractions/IFileManager.cs
@@ -1,9 +1,13 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 
 namespace Magenta.FileManager.Abstractions
 {
     public interface IFileManager
     {
         Task<IFileEntity> CreateAsync(IFileInput file);
+        Task<IFileEntity> UpdateAsync(Guid uniqueId, IFileInput fileInput);
+        Task<IFileEntity> GetAsync(Guid uniqueId);
+        Task<IFileEntity> DeleteAsync(Guid uniqueId);
     }
 }

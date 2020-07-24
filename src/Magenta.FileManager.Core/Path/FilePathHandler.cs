ccba65b8b45475e3bbaf5f65692fa044a5992911
Amirhossein Ghorbani <amirhosseinghorbani@hotmail.com>
2020-07-24 06:06:29 -0400

    Add file path handlers to handle paths

---
+++ src/Magenta.FileManager.Core/Path/FilePathHandler.cs
@@ -0,0 +1,13 @@
+﻿using Magenta.FileManager.Abstractions;
+using System;
+
+namespace Magenta.FileManager.Core.Path
+{
+    public class FilePathHandler : IFilePathHandler
+    {
+        public string GeneratePath(IFileEntity file)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/Magenta.FileManager.Core/Path/IFilePathHandler.cs
@@ -0,0 +1,9 @@
+﻿using Magenta.FileManager.Abstractions;
+
+namespace Magenta.FileManager.Core.Path
+{
+    public interface IFilePathHandler
+    {
+        string GeneratePath(IFileEntity file);
+    }
+}
